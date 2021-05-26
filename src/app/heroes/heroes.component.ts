d6ac059b00654692811062797c44d6f257a3674b
hhuumm <h4mid@Hotmail.com>
2021-05-26 16:20:15 -0400

    Angular Tour of Lists

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    if(this.selectedHero==hero)
+    {this.selectedHero=undefined}
+    else{
+      this.selectedHero = hero;
+    }
+    
+  }
 }
